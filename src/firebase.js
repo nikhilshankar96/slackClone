ec1b8ed055c9d06113f33a15a4b5ab3a3d1c35e0
Nikhil Shankar <nikhil_shankar@outlook.com>
2020-03-03 16:53:53 -0500

    added firebase and semantic-ui

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDD3euaCmkpSW2Ub_2tZAqPsRNrZzdqP34",
+	authDomain: "slack-clone-rfb.firebaseapp.com",
+	databaseURL: "https://slack-clone-rfb.firebaseio.com",
+	projectId: "slack-clone-rfb",
+	storageBucket: "slack-clone-rfb.appspot.com",
+	messagingSenderId: "311056762457",
+	appId: "1:311056762457:web:e9d6321a489ebc1bd09348",
+	measurementId: "G-DJ4MHGL3NB"
+};
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
@@ -9,13 +9,13 @@ import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+	<Router>
+		<Switch>
+			<Route exact path='/' component={App} />
+			<Route path='/login' component={Login} />
+			<Route path='/register' component={Register} />
+		</Switch>
+	</Router>
 );
 
 ReactDOM.render(<Root />, document.getElementById("root"));
